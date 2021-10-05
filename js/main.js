325472a8da403860421376c427eaeecba21dffce
vadim.shamshin <vadim.shamshin1@yandex.ru>
2021-10-05 17:46:54 +0300

    Сделал домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (max <= min) {
+function random(min, max) {
+  if (max > min) {
+    console.log('Функция работает');
+    //      5  +  от 0.1 до 0.9 * (99 - 5)
+    return Math.random() * (max - min) + min;
+  } else {
     console.log('Функция не работает');
+    return null
   }
-  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRandomArbitrary(min, max) {
-  if (max <= min) {
+random(0, 99)
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max > min) {
+    console.log('Функция работает');
+    return Math.floor(Math.random() * (max - min)) + min;
+  } else {
     console.log('Функция не работает');
-  }
-  return Math.random() * (max - min) + min;
+    return null
+  } 
 }
+
+random(0, 99)
+
+
+// lernjavascrirt.ru
+// почитать про функции, про циклы https://learn.javascript.ru/first-steps
+// посмотреть праивльную версию получения числа в диапазоне
+
 
 
